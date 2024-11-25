75b7d042355026ca2766027106d757a301a8655e
Dev Juu <devjuu@MacBook-Pro-khxng-Dev.local>
2024-11-25 08:24:02 +0700

    dashbord

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // กำหนด output folder
+  },
+  publicDir: 'public', // ให้แน่ใจว่า public directory จะถูกคัดลอกไปยัง build
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+});
